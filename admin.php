563f2304e2fba0a9c2517f24f2a4d0e84ce47d52
WahaabN <wahaabn@gmail.com>
2022-04-24 22:28:36 +0100

    admin sign up form

---
+++ admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="admin_sign_up.php" method = "post">
+        <input type="text" name="user_name" placeholder="Enter Username" />
+        <input type="text" name="user_email" placeholder="Enter Email" />
+        <input type="text" name="confirm_user_email" placeholder="Confirm Email" />
+        <input type="password" name = "user_password" placeholder="Enter Password" />
+        <input type= "password" name = "confirm_password" placeholder="Confirm Password" /> 
+        <button type = "submit" name = "sign_up_admin">Sign Up</button>
+     </form>
+</body>
+
+</html>
