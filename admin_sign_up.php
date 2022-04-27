a84d991a47df64885a32c4b78ce61d3f1deb1fd0
WahaabN <wahaabn@gmail.com>
2022-04-27 23:21:44 +0100

    update
    styled
    Sign out button implementation
    Conditional loading

--- admin_sign_up.php
+++ admin_sign_up.php
@@ -4,12 +4,27 @@
     if(isset($_POST["sign_up_admin"])){
         $username = htmlentities(mysqli_real_escape_string($con, $_POST['user_name']));
         $email = htmlentities(mysqli_real_escape_string($con, $_POST['user_email']));
+        $confirm_email = htmlentities(mysqli_real_escape_string($con, $_POST['confirm_user_email']));
         $password = md5(htmlentities(mysqli_real_escape_string($con, $_POST['user_password'])));
+        $confirm_password = md5(htmlentities(mysqli_real_escape_string($con, $_POST['confirm_password'])));
 
         $check_account_query = "SELECT * from admins WHERE admin_email = '$email'";
         $run_check = mysqli_query($con, $check_account_query);
 
         $accounts = mysqli_num_rows($run_check);
+
+        if ($email != $confirm_email) {
+            echo "<script>alert('Make sure emails match')</script>";
+            echo "<script>window.location.href = 'admin_sign_up_page.php';</script>";
+            exit();
+           
+        }
+        if($password != $confirm_password){
+            echo "<script>alert('Make sure passwords match')</script>";
+          echo "<script>window.location.href = 'admin_sign_up_page.php';</script>";
+            exit();
+            
+        }
 
         if ($accounts == 1){
             echo "<script>alert('Account with this email already exists')</script>";
